8a2b2f65ceaa6c603b745b436aff5360f444292f
Chanaka Sandaruwan <chanakasan@gmail.com>
2014-02-08 18:16:33 +0530

    Create get favorite item route

--- src/Dream89/MainBundle/Controller/FavoriteController.php
+++ src/Dream89/MainBundle/Controller/FavoriteController.php
@@ -1,8 +1,10 @@
 <?php
 namespace Dream89\MainBundle\Controller;
 
+use Dream89\MainBundle\Document\FavoriteItem;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use FOS\RestBundle\Controller\Annotations\View;
+use FOS\RestBundle\Controller\Annotations\View,
+    FOS\RestBundle\Controller\Annotations\RouteResource;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 
 class FavoriteController extends Controller {
@@ -18,4 +20,16 @@ class FavoriteController extends Controller {
             ->findAll();
         return array('favorites' => $favorites );
     }
-} 
+
+    /**
+     * @return array
+     * @View()
+     */
+    function getFavoriteAction(FavoriteItem $favoriteItem)
+    {
+        $data = $this->get('doctrine_phpcr')->getManager()
+            ->getRepository('Dream89\MainBundle\Document\FavoriteItem')
+            ->find('Dream89\MainBundle\Document\FavoriteItem', $favoriteItem);
+        return array('favorites' => $data );
+    }
+}
