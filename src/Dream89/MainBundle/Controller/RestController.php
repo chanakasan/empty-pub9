f795987a8ca7dcbb0a5d2c5d49ee6fa09a8419e0
Chanaka Sandaruwan <chanakasan@gmail.com>
2014-02-09 20:08:51 +0530

    Add _create_favorite route, small refactorings

--- src/Dream89/MainBundle/Controller/RestController.php
+++ src/Dream89/MainBundle/Controller/RestController.php
@@ -1,6 +1,7 @@
 <?php
 namespace Dream89\MainBundle\Controller;
 
+use Dream89\MainBundle\Document\FavoriteItem;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Config\Definition\Exception\Exception;
 
@@ -62,17 +63,53 @@ class RestController extends Controller {
         $this->_checkResult($result, $favoriteItem);
         $dm->remove($result);
         $dm->flush();
-        $data = array(
-            'message' => sprintf("Item '%s' removed.", $result->getName()),
-            'success' => true,
-        );
-        echo json_encode($data);
-        exit;
+        $this->_sendSuccessMsg(sprintf("Item '%s' removed.", $result->getName()));
     }
 
     /**
-     * Delete a record
-     * POST | DELETE
+     * Create new item
+     */
+    function createAction()
+    {
+        $dm = $this->get('doctrine_phpcr')->getManager();
+        $favorite = new FavoriteItem();
+        $favorite = $this->_createItem($favorite);
+
+        try{
+            $dm->persist($favorite);
+            $dm->flush();
+        } catch(\PHPCR\ItemExistsException $e) {
+            $this->_sendFailedMsg(sprintf('Item %s already exists', $favorite->getName()));
+        }
+        $this->_sendSuccessMsg(sprintf("Item '%s' created.", $favorite->getName()));
+    }
+
+
+    private function _createItem($favorite)
+    {
+        $name = $this->getRequest()->get('name');
+        $url = $this->getRequest()->get('url');
+        $tags = $this->getRequest()->get('tags');
+
+        if($name === null || $url === null)
+        {
+            $this->_sendFailedMsg('name or url params not set.');
+        }
+        /**
+         * @var $favorite \Dream89\MainBundle\Document\FavoriteItem
+         */
+        $favorite->setName($name);
+        $favorite->setUrl($url);
+        $favorite->setTags($tags);
+
+        return $favorite;
+    }
+
+
+
+    /**
+     * Update a record
+     * POST | PUT
      */
     function updateAction($favoriteItem)
     {
@@ -88,43 +125,7 @@ class RestController extends Controller {
 
         $dm->persist($result);
         $dm->flush();
-
-        $data = array(
-            'message' => sprintf("Item '%s' removed.", $result->getName()),
-            'success' => true,
-        );
-        echo json_encode($data);
-        exit;
-    }
-
-    private function _checkResult($result, $favoriteItem)
-    {
-        try {
-            if($result === null)
-            {
-                throw new Exception(sprintf("Item '%s' not found.", $favoriteItem));
-            }
-        } catch(Exception $e) {
-            $data = array(
-                'error' => $e->getMessage(),
-                'success' => false,
-            );
-            echo json_encode($data);
-            exit;
-        }
-    }
-
-    private function _serialize($obj)
-    {
-        /**
-         * @var $obj \Dream89\MainBundle\Document\FavoriteItem
-         */
-        return array(
-            'id' => $obj->getId(),
-            'name' => $obj->getName(),
-            'url' => $obj->getUrl(),
-            'tags' => $obj->getTags(),
-        );
+        $this->_sendSuccessMsg(sprintf("Item '%s' removed.", $result->getName()));
     }
 
     private function _updateResult($result)
@@ -146,4 +147,48 @@ class RestController extends Controller {
         return $result;
     }
 
+    private function _checkResult($result, $favoriteItem)
+    {
+        try {
+            if($result === null)
+            {
+                throw new Exception(sprintf("Item '%s' not found.", $favoriteItem));
+            }
+        } catch(Exception $e) {
+            $this->_sendFailedMsg($e->getMessage());
+        }
+    }
+
+    private function _serialize($obj)
+    {
+        /**
+         * @var $obj \Dream89\MainBundle\Document\FavoriteItem
+         */
+        return array(
+            'id' => $obj->getId(),
+            'name' => $obj->getName(),
+            'url' => $obj->getUrl(),
+            'tags' => $obj->getTags(),
+        );
+    }
+
+    private function _sendFailedMsg($msg)
+    {
+        $data = array(
+            'error' => $msg,
+            'success' => false,
+        );
+        echo json_encode($data);
+        exit;
+    }
+
+    private function _sendSuccessMsg($msg)
+    {
+        $data = array(
+            'message' => $msg,
+            'success' => true,
+        );
+        echo json_encode($data);
+        exit;
+    }
 } 
